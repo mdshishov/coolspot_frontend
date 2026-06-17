cca72886cdd1b3a9581c34e98ce95bd5b9beef4a
Mikhail Shishov <mdshishov@mail.ru>
2026-06-17 11:05:58 +0300

    fix

--- src/shared/utils/resolveMediaUrl.ts
+++ src/shared/utils/resolveMediaUrl.ts
@@ -1,6 +1,6 @@
 export function resolveMediaUrl(url?: string) {
   if (!url) return "";
-  return url.startsWith("http")
+  return url.startsWith("https")
     ? url
     : `${import.meta.env.VITE_MEDIA_URL}${url}`;
 }
